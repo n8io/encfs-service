0cecd84dd181e45c1513d50831be77bf5032731c
Nate Clark <n8@n8io.com>
2021-05-15 18:05:04 -0400

    ✨ Add execution duration

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-import util from 'util';
 import { exec as execCb } from 'child_process';
-import express from 'express';
 import dotenv from 'dotenv-safe';
+import express from 'express';
+import prettyMs from 'pretty-ms';
+import util from 'util';
 
 dotenv.config();
 
@@ -29,17 +30,24 @@ const app = express();
 
 app.use(express.json());
 
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
+  return next();
 });
 
-app.get('/', async (req, res) => {
+app.use((req,_res,next) => {
+  req.start = new Date();
+  return next();
+});
+
+app.get('/', async (req, res, next) => {
   const { p: path } = req.query;
 
   if (!path) {
-    return res.status(400).json({ error: `querystring 'p' was not provided` });
+    res.locals.error =  `querystring 'p' was not provided` ;
+
+    return next()
   }
 
   const paths = getPathArray(path);
@@ -52,14 +60,17 @@ app.get('/', async (req, res) => {
     res.status(500);
   }
 
-  return res.json({ timestamp: new Date(), results });
+  res.locals.results = results;
+
+  return next()
 });
 
 const isValidBody = paths => Array.isArray(paths) && paths.length > 0 && !paths.find(p => typeof p !== 'string');
 
-app.post('/', async (req, res) => {
+app.post('/', async (req, res, next) => {
   const paths = req.body;
 
+  // eslint-disable-next-line no-console
   console.log(paths);
 
   if (!isValidBody(paths)) {
@@ -74,7 +85,18 @@ app.post('/', async (req, res) => {
     res.status(500);
   }
 
-  return res.json({ timestamp: new Date(), results });
+  res.locals.results = results;
+
+  return next();
+});
+
+
+app.use((req,res) => {
+  const { start } = req;
+  const now = new Date();
+  const duration =prettyMs(now.getTime() - start.getTime());
+
+  return res.json({ results: res.locals, duration, timestamp: new Date() });
 });
 
 const { PORT = 3000 } = process.env; // eslint-disable-line no-process-env
